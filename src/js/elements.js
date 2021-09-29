1eafe72a7f0ca849563558116b8f377845e5bce4
Den Zubr <fort_zd@mail.ru>
2021-09-29 20:52:31 +0700

    datepicker

---
+++ src/js/elements.js
@@ -0,0 +1,20 @@
+$('#calendar').datepicker({
+    position: "bottom right",
+    range: true,
+    minDate: new Date(),
+    inline: true,
+    navTitles: {
+        days: 'MM yyyy'
+    },
+    onSelect(formattedDate, date, inst) {
+        month_1 = parseInt(date[0].getMonth(), 10) + 1;
+        month_2 = parseInt(date[1].getMonth(), 10) + 1;
+        $('#date_1').val(date[0].getDate() + '.' + month_1 + '.' + date[0].getFullYear());
+        $('#date_2').val(date[1].getDate() + '.' + month_2 + '.' + date[1].getFullYear());
+    }
+})
+
+
+
+
+
