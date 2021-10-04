f4d2f8ed18064234b1cb401eef2345f293ec3753
Den Zubr <fort_zd@mail.ru>
2021-10-04 17:04:11 +0700

    Приступаю к слайдеру

--- src/js/elements.js
+++ src/js/elements.js
@@ -32,5 +32,13 @@ $(function() {
     $('.iqdropdown').iqDropdown();
 })
 
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 500,
+    grid: true
+});
 
 
