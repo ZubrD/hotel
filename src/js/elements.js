285b28ae3c530888c9bdb70f4445046a93b5a423
Den Zubr <fort_zd@mail.ru>
2021-10-05 21:58:55 +0700

    Добавил блок выбора диапазона цены, синхронизировал слайдер с инпутами

--- src/js/elements.js
+++ src/js/elements.js
@@ -32,13 +32,5 @@ $(function() {
     $('.iqdropdown').iqDropdown();
 })
 
-$(".js-range-slider").ionRangeSlider({
-    type: "double",
-    min: 0,
-    max: 1000,
-    from: 200,
-    to: 500,
-    grid: true
-});
 
 
