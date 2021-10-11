54c0dd10ae5b42ba96eda596873eed47cc28d442
Den Zubr <fort_zd@mail.ru>
2021-10-11 17:05:22 +0700

    Добавил блок Дополнительные удобства

---
+++ src/js/additional.js
@@ -0,0 +1,9 @@
+$('.additional__title').on('click', function(){
+    $('.additional__icon').toggleClass('rotated__icon')
+    $('.additional-block').toggleClass('block-open')
+})
+
+$('.additional__icon').on('click', function(){
+    $(this).toggleClass('rotated__icon')
+    $('.additional-block').toggleClass('block-open')
+})
