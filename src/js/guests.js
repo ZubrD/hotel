42931c28beea5fa54aa2f0c9c3493c78e7a9232b
Den Zubr <fort_zd@mail.ru>
2022-03-21 13:43:17 +0700

    ограничение по количеству взрослых, детей, младенцев; необходимость сопровождения детей и младенцев хотя бы одним взрослым

--- src/js/guests.js
+++ src/js/guests.js
@@ -19,24 +19,32 @@ $('.plus-adult').on('click', function(event){
   if (value > 0) {
       $('.minus-adult').css('border', '1px solid rgba(31, 32, 65, 0.25)')
   }
-  $('.guest__input').val(my_select())
+  $('.guest__input').val(messangeInput())
   $('.adult-result').text(value)
+  messangeInput()
 })
 
 $('.minus-adult').on('click', function(event){
   event.preventDefault()
   let value = $('.guest__input').attr('adult')
   let guests = parseInt($('.guest__input').attr('guests'))
+  let child = parseInt($('.guest__input').attr('child'))
+
+  
   if (value != 0) {
+
+    /* Если есть дети, то должен остаться хоть один взрослый */
+    if (!(value == 1 && guests > 1)) {
       value = value - 1
       guests = guests - 1
       $('.guest__input').attr({'adult': value})         
-      $('.guest__input').attr({'guests': guests})         
+      $('.guest__input').attr({'guests': guests})       
+    }
   } 
   if (value == 0) {
         $(this).css('border', '1px solid rgba(31, 32, 65, 0.1)')
   }
-  $('.guest__input').val(my_select())
+  $('.guest__input').val(messangeInput())
   $('.adult-result').text(value)
 })
 
@@ -46,17 +54,29 @@ $('.plus-child').on('click', function(event){
   event.preventDefault()
   let value = parseInt($('.guest__input').attr('child'))
   let guests = parseInt($('.guest__input').attr('guests'))
-  if (guests < 10) {
-      value = value + 1
+  let adult = parseInt($('.guest__input').attr('adult'))
+
+  /* Не более 5 детей */
+  if (value < 5 && guests < 10) {
+
+  /* Должен быть как минимум один взрослый */
+    if ( adult === 0) {
+      adult = 1
       guests = guests + 1
-      $('.guest__input').attr({'child': value})        
-      $('.guest__input').attr({'guests': guests})        
+    }
+
+    value = value + 1
+    guests = guests + 1
+    $('.guest__input').attr({'child': value})        
+    $('.guest__input').attr({'guests': guests})        
+    $('.guest__input').attr({'adult': adult})        
   }
   if (value > 0) {
       $('.minus-child').css('border', '1px solid rgba(31, 32, 65, 0.25)')
   }    
-  $('.guest__input').val(my_select())
+  $('.guest__input').val(messangeInput())
   $('.child-result').text(value)
+  $('.adult-result').text(adult)
 })
 
 $('.minus-child').on('click', function(event){
@@ -72,7 +92,7 @@ $('.minus-child').on('click', function(event){
   if (value == 0) {
       $(this).css('border', '1px solid rgba(31, 32, 65, 0.1)')
   }     
-  $('.guest__input').val(my_select())
+  $('.guest__input').val(messangeInput())
   $('.child-result').text(value)
 })
 
@@ -82,17 +102,29 @@ $('.plus-baby').on('click', function(event){
   event.preventDefault()
   let value = parseInt($('.guest__input').attr('baby'))
   let guests = parseInt($('.guest__input').attr('guests'))
-  if (value < 5 && guests < 10) {
-      value = value + 1
+  let adult = parseInt($('.guest__input').attr('adult'))
+
+  /* Не более 2 младенцев */
+  if (value < 2 && guests < 10) {
+
+    /* Должен быть как минимум один взрослый */
+    if ( adult === 0) {
+      adult = 1
       guests = guests + 1
-      $('.guest__input').attr({'baby': value})        
-      $('.guest__input').attr({'guests': guests})        
+    }
+
+    value = value + 1
+    guests = guests + 1
+    $('.guest__input').attr({'baby': value})        
+    $('.guest__input').attr({'guests': guests})  
+    $('.guest__input').attr({'adult': adult})      
   }
   if (value > 0) {
       $('.minus-baby').css('border', '1px solid rgba(31, 32, 65, 0.25)')
   }    
-  $('.guest__input').val(my_select())
+  $('.guest__input').val(messangeInput())
   $('.baby-result').text(value)
+  $('.adult-result').text(adult)
 })
 
 $('.minus-baby').on('click', function(event){
@@ -108,58 +140,42 @@ $('.minus-baby').on('click', function(event){
   if (value == 0) {
       $(this).css('border', '1px solid rgba(31, 32, 65, 0.1)')
   }    
-  $('.guest__input').val(my_select())
+  $('.guest__input').val(messangeInput())
   $('.baby-result').text(value)
 })
 
-function my_select() {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function messangeInput() {
+  console.log('вызов')
     let guests = parseInt($('.guest__input').attr('guests'))
-    // let adult = parseInt($('.guest__input').attr('adult'))
-    // let child = parseInt($('.guest__input').attr('child'))
     let baby = parseInt($('.guest__input').attr('baby'))
-    let sleep_pl, bed_pl, bath_pl
-
-    switch (guests) {
-        case 1: sleep_pl = 'гость'; break;
-        case 2: sleep_pl = 'гостя'; break;
-        case 3: sleep_pl = 'гостей'; break;
-    }
-
-    // switch (bed) {
-    //     case 1: bed_pl = 'кровать'; break;
-    //     case 2: bed_pl = 'кровати'; break;
-    //     case 3: bed_pl = 'кровати'; break;
-    //     case 4: bed_pl = 'кровати'; break;
-    //     case 5: bed_pl = 'кроватей'; break;
-    //     case 6: bed_pl = 'кроватей'; break;
-    // }
-
-    switch (baby) {
-        case 1: bath_pl = 'младенец'; break;
-        case 2: bath_pl = 'младенца'; break;
-        case 3: bath_pl = 'младенцев'; break;
-    }
+    let guestsPl, babyPl
+    let messange
+    let onlyGuests = guests - baby
     
-    if (sleep == 0){
-        sleep_ms = ''  
-    } else if (bed != 0 || bath != 0) {
-        sleep_ms = sleep + ' ' + sleep_pl + ', '
-    } else {
-        sleep_ms = sleep + ' ' + sleep_pl
+    if (onlyGuests == 1){
+        sleep_ms = '' 
+        guestsPl = 'гость' 
+    } else if (onlyGuests > 1 && onlyGuests < 5) {
+        guestsPl = 'гостя'
+    } else if (guests > 4) {
+        guestsPl = 'гостей'
     }
 
-    if (bed == 0){
-        bed_ms = ''
-    } else if (bath != 0) {
-        bed_ms = bed + ' ' + bed_pl + ', '
-    } else {
-        bed_ms = bed + ' ' + bed_pl
+    if (baby == 1){
+        babyPl = 'младенец'
+    } else if (baby > 1 && baby < 5) {
+        babyPl = 'младенца'
+    } else if (baby > 4) {
+        babyPl = 'младенцев'
     }
-    
-    if (bath == 0){
-        bath_ms = ''
-    } else {
-        bath_ms = bath + ' ' + bath_pl
-    }    
-    return sleep_ms + bed_ms  + bath_ms
+
+    if (onlyGuests > 0 && baby == 0) {
+      messange = onlyGuests + ' ' + guestsPl
+    } else if (onlyGuests > 0 && baby > 0) {
+      messange = onlyGuests + ' ' + guestsPl + ', ' + baby + ' ' + babyPl
+    }
+ 
+    return messange
 }
